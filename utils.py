a7d138e3306cfbff1c008ab9fae87c1cefc93c04
Luan Ademi <26838000+LuanAdemi@users.noreply.github.com>
2022-01-10 23:35:02 +0100

    Normalized the probabilities just in case

--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ import numpy as np
 def rand_choice_nb(arr, prob):
     """
     See https://github.com/numba/numba/issues/2539
+    This also normalizes the probabilities in case they are non-probabalistic.
     :param arr: A 1D numpy array of values to sample from.
     :param prob: A 1D numpy array of probabilities for the given samples.
     :return: A random sample from the given array with a given probability.
     """
-    return arr[np.searchsorted(np.cumsum(prob), np.random.random(), side="right")]
+    return arr[np.searchsorted(np.cumsum(prob/np.sum(prob)), np.random.random(), side="right")]
